a5c2163110be0b6e781de0f54dd3c3380e7f649d
runningman7021 <runningman7021@163.com>
2018-09-23 10:14:42 +0800

    cloud后台管理系统:cloud-eureka-server:服务注册中心添加用户认证后添加的配置类

---
+++ cloud-eureka-server/src/main/java/com/runningman/cloud/cloudeurekaserver/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.runningman.cloud.cloudeurekaserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//解决eurekaserver开启用户认证后其他服务无法注册的问题
+//类名必须一致：WebSecurityConfig
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); //关闭csrf
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); //开启认证
+    }
+
+
+
+
+}
